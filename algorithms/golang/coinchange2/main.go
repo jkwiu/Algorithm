217e931df166ece3de2204acd3db3989a93f9ada
sizzflyer <sizzflyer@gmail.com>
2020-10-21 09:08:48 +0900

    dynamic programing coin change

---
+++ algorithms/golang/coinchange2/main.go
@@ -0,0 +1,27 @@
+// 518. Coin Change 2
+// You are given coins of different denominations and a total amount of money.
+// Write a function to compute the number of combinations that make up that amount.
+// You may assume that you have infinite number of each kind of coin.
+
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1,3,5,7}
+	change(10, arr)
+}
+
+func change(amount int, coins []int) int {
+	arr := make([]int, amount+1)
+	arr[0] = 1
+	for _, c := range coins {
+		for i := 0; i < amount; i++ {
+			if i+c <= amount {
+				arr[i+c] += arr[i]
+			}
+		}
+	}
+	fmt.Println(arr)
+	return arr[amount]
+}
